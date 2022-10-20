1aa3cbd3b951abbf80724ed7dd7f948fa3b711cf
bicky <bickybong@gmail.com>
2022-10-20 10:54:54 +0800

    done with part 1

--- client/src/components/posts/post/post.js
+++ client/src/components/posts/post/post.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import useStyles from './styles';
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import useStyles from "./styles";
 
-const Post = () =>{
-    const classes = useStyles();
-    return (
-        <h1>POST</h1>
-    )
-}
+const Post = () => {
+  const classes = useStyles();
+  return <h1>POST</h1>;
+};
 
 export default Post;
